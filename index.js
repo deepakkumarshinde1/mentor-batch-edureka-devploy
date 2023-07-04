65e7cb7797457c7ab5d8c9803ae551a02736006a
deepakkumarshinde1 <deepakkumarshinde1@gmail.com>
2023-07-04 18:51:38 +0530

    sample

---
+++ index.js
@@ -0,0 +1,43 @@
+// handel project logic  in a structured way
+// application user
+
+class User {
+  // add user
+  add() {}
+  // delete user
+  delete() {}
+  // edit user
+  edit() {}
+  // read user
+  print() {}
+  // user login
+  login() {}
+  // user logout
+  logout() {}
+
+  addCovidDetails() {}
+}
+
+let student = new User();
+student.add();
+
+let admin = new User();
+
+let manager = new User();
+manager.print();
+
+alert();
+console.log();
+
+// var let const
+// OOPs
+// Destructuring
+// Spread
+// Template String
+// Rest Para
+// Async Await
+// Fetch API
+
+// PROMISE
+// api call
+// async (op1 => op2 =>  op3 => op4)
